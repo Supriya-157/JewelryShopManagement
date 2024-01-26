1047047cd40e4e17abde8719cd7a671767dc6f9f
Supriya-157 <56032946+Supriya-157@users.noreply.github.com>
2024-01-26 17:43:37 +0530

    Add files via upload

---
+++ JewelryManagement.webapi/JewelryManagement.webapi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace JewelryManagement.webapi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ JewelryManagement.webapi/JewelryManagement.webapi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JewelryManagement.webapi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
